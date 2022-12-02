ca4852a1e1d775427d8415882570f2787a38f4ed
Marcus Koh <kohchihao@gmail.com>
2022-12-02 15:49:35 +0800

    feat: add progress bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import ProgressBar from './ProgressBar';
 
 export default function App() {
+  const countInterval = useRef<any>(null);
+  const [count, setCount] = useState(0);
+
+  const loaderValue = useRef(new Animated.Value(0)).current;
+
+  const load = (count) => {
+    Animated.timing(loaderValue, {
+      toValue: count, //final value
+      duration: 100, //update value in 500 milliseconds
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    countInterval.current = setInterval(
+      () => setCount((old) => old + 0.1),
+      500
+    );
+    return () => {
+      clearInterval(countInterval.current); //when user exits, clear this interval.
+    };
+  }, []);
+
+  useEffect(() => {
+    load(count);
+    if (count >= 1) {
+      setCount(0);
+    }
+  }, [count]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{count}</Text>
+
+      <View style={{ width: '50%' }}>
+        <ProgressBar progress={0.6} />
+      </View>
+
+      <ProgressBar progress={0} />
+      <ProgressBar progress={100} />
+      <ProgressBar progress={count} />
     </View>
   );
 }
@@ -14,7 +54,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
+  },
+  progressTrack: {
+    height: 4,
+    flexDirection: 'row',
+    width: '100%',
+    backgroundColor: '#f0f0f0',
   },
 });
---
+++ ProgressBar.tsx
@@ -0,0 +1,73 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  LayoutChangeEvent,
+} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+
+type Props = {
+  progress?: number;
+  indeterminate?: boolean;
+};
+
+const ProgressBar = ({ progress = 0 }: Props) => {
+  const [deviceWidth, setDeviceWidth] = useState(0);
+  const loaderValue = useRef(new Animated.Value(0)).current;
+
+  const onProgressTrackLayout = (event: LayoutChangeEvent) => {
+    setDeviceWidth(event.nativeEvent.layout.width);
+  };
+
+  useEffect(() => {
+    Animated.timing(loaderValue, {
+      toValue: progress,
+      duration: 100,
+      useNativeDriver: true,
+      isInteraction: false,
+    }).start();
+  }, [progress]);
+
+  return (
+    <View style={styles.progressTrack} onLayout={onProgressTrackLayout}>
+      <Animated.View
+        style={[
+          styles.progressBar,
+          {
+            transform: [
+              {
+                translateX: loaderValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-1 * 0.5 * deviceWidth, 0],
+                }),
+              },
+              {
+                scaleX: loaderValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0.0001, 1],
+                }),
+              },
+            ],
+          },
+        ]}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  progressTrack: {
+    height: 4,
+    flexDirection: 'row',
+    width: '100%',
+    backgroundColor: '#f0f0f0',
+    overflow: 'hidden',
+  },
+  progressBar: {
+    flex: 1,
+    backgroundColor: '#8BED4F',
+  },
+});
+
+export default ProgressBar;
